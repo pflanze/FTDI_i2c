c7d3b23f78587e395e272335cbfc0fdb6cc0f3ca
Christian Jaeger <ch@christianjaeger.ch>
2020-09-09 13:14:42 +0000

    selfH.pl: change tests to use test strings instead of real files

--- selfH.pl
+++ selfH.pl
@@ -72,7 +72,54 @@ sub file_prototypes($path, $c_or_h) {
                       $c_or_h)
 }
 
-TEST { [file_prototypes  "bridge.h","h"] }
+my $tst_bridge_h= q'
+#include "../ftd2xx.h"
+
+DWORD dev_createInfo(void);
+FT_DEVICE_LIST_INFO_NODE*  dev_getInfo(void);
+FT_HANDLE* dev_open(void);
+int dev_close(void);
+// int dev_init(void);
+
+';
+
+my $tst_bridge_c= '
+/*------------------------Populates device information
+Notes:
+    Call before 
+*/
+DWORD dev_createInfo(abr) {
+    // create the device information list
+    DWORD numDevs;
+...
+}
+/* ..
+    ftHandle=Null >>> indicates that a handle has not yet been created or open
+*/
+
+FT_DEVICE_LIST_INFO_NODE* dev_getInfo(void) { 
+    DWORD numDevs = dev_createInfo();      
+...
+}
+//------------------------------------Device Initialization && termination
+
+FT_HANDLE* dev_open(void) {
+    FT_HANDLE* ftHandle = malloc(sizeof(FT_HANDLE));// Handle of the FTDI device
+    ...
+}
+
+//Not finished
+int dev_close(void) {
+    //free data
+}
+
+//------------------------------------
+// int dev_init(void){
+//    ...
+// }
+';
+
+TEST { [ string_prototypes $tst_bridge_h, "h" ] }
 [
  'DWORD dev_createInfo(void);',
  'FT_DEVICE_LIST_INFO_NODE*  dev_getInfo(void);',
@@ -80,7 +127,7 @@ TEST { [file_prototypes  "bridge.h","h"] }
  'int dev_close(void);'
 ];
 
-TEST { [file_prototypes  "bridge.c","c"] }
+TEST { [ string_prototypes $tst_bridge_c, "c" ] }
 [
  'DWORD dev_createInfo(abr);',
  'FT_DEVICE_LIST_INFO_NODE* dev_getInfo(void);',
