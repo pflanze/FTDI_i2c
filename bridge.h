e9b425a7784b2efd6933b95faf469f9d66c78c80
Christian Jaeger <ch@christianjaeger.ch>
2020-09-11 00:37:11 +0000

    Update bridge.h to rely on bridge_prototypes.h

--- bridge.h
+++ bridge.h
@@ -1,8 +1,2 @@
 #include "../ftd2xx.h"
-
-DWORD dev_createInfo(void);
-FT_DEVICE_LIST_INFO_NODE*  dev_getInfo(void);
-FT_HANDLE* dev_open(void);
-int dev_close(void);
-// int dev_init(void);
-
+#include "bridge_prototypes.h"
